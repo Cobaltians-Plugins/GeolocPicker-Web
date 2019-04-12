26b9c1cd7a5ecf6090765e824e42078acc8b1d29
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 15:46:11 +0200

    use cobalt.plugins.send(this,action,data,callback)

--- cobalt.geolocpicker.js
+++ cobalt.geolocpicker.js
@@ -9,11 +9,8 @@
             cobalt.log(this.name, ' plugin : unknown event received :', json)
         },
 		selectLocation: function(params, callback){
-			this.send('selectLocation', params, callback)
-		},
-        send:function(action, data, callback){
-            cobalt.send({ type : "plugin", name : this.name, action : action, data : data }, callback);
-        }
+			cobalt.plugins.send(this, 'selectLocation', params, callback)
+		}
     };
     cobalt.plugins.register(plugin);
 })(cobalt || {});
